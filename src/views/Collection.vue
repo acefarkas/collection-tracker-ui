96f200bb3254bcb6bb51a16079aeda019e5a6389
Andrew <acefarkas@gmail.com>
2021-08-22 20:17:15 -0400

    feat: 🎸 added temporary collection table

---
+++ src/views/Collection.vue
@@ -0,0 +1,15 @@
+<template>
+  <collection-table />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import CollectionTable from '@/components/CollectionTable.vue';
+
+@Component({
+  components: {
+    CollectionTable,
+  },
+})
+export default class Collection extends Vue {}
+</script>
